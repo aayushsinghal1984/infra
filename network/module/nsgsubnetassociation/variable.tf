78d76f2b5debc6e3e704598f883be075b0bbb38d
aayushsinghal1984 <aayush.singhal01@gmail.com>
2026-06-13 09:14:04 +0530

    Update variable.tf

--- network/module/nsgsubnetassociation/variable.tf
+++ network/module/nsgsubnetassociation/variable.tf
@@ -1,8 +1,8 @@
 variable "nsgsubnetassociation" {
-    description = "A map of Subnet and Network Security Group IDs to associate them together."
-    type = map(object({
-    subnet_id                 = string
-    network_security_group_id = string
+  type = map(object({
+    subnet_name                 = string 
+    virtual_network_name        = string
+    network_security_group_name = string
+    resource_group_name         = string
   }))
- 
 }
